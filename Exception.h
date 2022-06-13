68c0a8d7bed30d3aca328c008d9bb739a3275d76
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-13 18:44:26 +0300

    v 0.3 Added Mtmchkin.cpp and Exception.h Files

---
+++ Exception.h
@@ -0,0 +1,8 @@
+//
+// Created by omarz on 13/06/2022.
+//
+
+#ifndef EX4_EXCEPTION_H
+#define EX4_EXCEPTION_H
+
+#endif //EX4_EXCEPTION_H
---
+++ Mtmchkin.cpp
@@ -0,0 +1,4 @@
+//
+// Created by omarz on 13/06/2022.
+//
+
