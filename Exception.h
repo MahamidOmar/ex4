b44132a288ffabb826aede0cd18bdf2df1ebac4a
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-21 00:19:25 +0300

    v 3.0

--- Exception.h
+++ Exception.h
@@ -8,15 +8,28 @@
 class DeckFileNotFound : public std::exception
 {
 public:
-    const char* what() const noexcept override;
+    const char* what() const noexcept override
+    {
+        return "Deck File Error: File not found";
+    }
 };
 
 class DeckFileFormatError : public std::exception
 {
 public:
-    DeckFileFormatError(int line);
-    ~DeckFileFormatError();
-    const char* what() const noexcept override;
+    DeckFileFormatError(int line): m_line(line)
+    {
+        m_errorMessage = new std::string ("Deck File Error: File format error in line ");
+        *m_errorMessage += std::to_string(m_line);
+    }
+    ~DeckFileFormatError() override
+    {
+        delete m_errorMessage;
+    }
+    const char* what() const noexcept override
+    {
+        return m_errorMessage->c_str();
+    }
 private:
     std::string* m_errorMessage;
     int m_line;
@@ -25,35 +38,42 @@ private:
 class DeckFileInvalidSize : public std::exception
 {
 public:
-    const char* what() const noexcept override;
+    const char* what() const noexcept override
+    {
+        return "Deck File Error: Deck size is invalid";
+    }
 };
 
 
 ////implementations
-const char* DeckFileNotFound::what() const noexcept
-{
-    return "Deck File Error: File not found";
-}
+//1
+//const char* DeckFileNotFound::what() const noexcept
+//{
+//    return "Deck File Error: File not found";
+//}
 
-DeckFileFormatError::DeckFileFormatError(int line) : m_line(line)
-{
-    m_errorMessage = new std::string ("Deck File Error: File format error in line ");
-    *m_errorMessage += std::to_string(m_line);
-}
-const char* DeckFileFormatError::what() const noexcept
-{
-    return m_errorMessage->c_str();
-}
+//2
+//DeckFileFormatError::DeckFileFormatError(int line) : m_line(line)
+//{
+//    m_errorMessage = new std::string ("Deck File Error: File format error in line ");
+//    *m_errorMessage += std::to_string(m_line);
+//}
 
-DeckFileFormatError::~DeckFileFormatError()
-{
-    delete m_errorMessage;
-}
+//const char* DeckFileFormatError::what() const noexcept
+//{
+//    return m_errorMessage->c_str();
+//}
 
-const char* DeckFileInvalidSize::what() const noexcept
-{
-    return "Deck File Error: Deck size is invalid";
-}
+//DeckFileFormatError::~DeckFileFormatError()
+//{
+//    delete m_errorMessage;
+//}
+
+//3
+//const char* DeckFileInvalidSize::what() const noexcept
+//{
+//    return "Deck File Error: Deck size is invalid";
+//}
 
 
 
