14123384d26eefbc85ce716fa4b69eef51fbe1c8
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-23 16:29:46 +0300

    v 6.4

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -126,43 +126,6 @@ void readSize(int& size)
 //    }
 }
 
-//int fillPlayersDeque(std::deque<std::unique_ptr<Player>>& players)
-//{
-//    int size;
-//    printStartGameMessage();
-//    printEnterTeamSizeMessage();
-//    readSize(size);
-//    for(int i = 0 ; i < size ; i++)
-//    {
-//        printInsertPlayerMessage();
-//
-//        std::string player_name;
-//        std::cin >> player_name;
-//
-//        std::string player_class;
-//        std::cin >> player_class;
-//        bool flag = checkPlayerInvalid(player_name) && checkClassInvalid(player_class);
-//        while(!flag)
-//        {
-//            if(!checkPlayerInvalid(player_name))
-//            {
-//                printInvalidName();
-//                std::cin >> player_name;
-//                std::cin >> player_class;
-//            }
-//            else
-//            {
-//                printInvalidClass();
-//                std::cin >> player_name;
-//                std::cin >> player_class;
-//            }
-//            flag = checkPlayerInvalid(player_name) && checkClassInvalid(player_class);
-//        }
-//        players.push_back(std::unique_ptr<Player>(getPlayerType(player_class, player_name)));
-//    }
-//    return size;
-//}
-
 int fillPlayersDeque(std::deque<std::unique_ptr<Player>>& players)
 {
     int size;
---
+++ tool/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by omarz on 23/06/2022.
+//
+
+#include "../Mtmchkin.h"
+
+int main()
+{
+    try{
+        Mtmchkin game("Deck.txt");
+        while(!game.isGameOver())
+        {
+            game.playRound();
+        }
+        game.printLeaderBoard();
+    } catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return 0;
+    }
+    return 1;
+}
